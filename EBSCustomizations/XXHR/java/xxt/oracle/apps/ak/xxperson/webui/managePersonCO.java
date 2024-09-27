abbbc92d87758b39900d2b44c30869e5f9451e8b
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2024-09-27 07:12:38 +0530

    Update managePersonCO.javasfqwefsdafqwef weffqw
    wecasd
    
    wefsd
    wefsd

--- EBSCustomizations/XXHR/java/xxt/oracle/apps/ak/xxperson/webui/managePersonCO.java
+++ EBSCustomizations/XXHR/java/xxt/oracle/apps/ak/xxperson/webui/managePersonCO.java
@@ -21,6 +21,7 @@ import oracle.apps.fnd.framework.OAException;
  */
 //comment1
 //comment2
+//comment3
 public class managePersonCO extends OAControllerImpl
 {
   public static final String RCS_ID="$Header$";
