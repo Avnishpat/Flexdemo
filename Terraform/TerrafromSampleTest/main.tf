cfc9013d9e05d83a3ac379a6e1cf2ed1eed1c1df
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2025-03-31 14:59:49 +0530

    Update main.tf

--- Terraform/TerrafromSampleTest/main.tf
+++ Terraform/TerrafromSampleTest/main.tf
@@ -1,9 +1,13 @@
+terraform {
+    
+	backend "s3" {}
+}
 provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "instance1" {
   ami           = "ami-08a0d1e16fc3f61ea"
-  instance_type = "t2.micro"
+  instance_type="demo"
 
   tags = {
     name = "My-Demo-Instances-Demo"
