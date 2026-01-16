3b4dd131e6b73b5a7bfad52f8f7455bd35f51281
Avnish Patel <110018459+Avnishpat@users.noreply.github.com>
2026-01-16 15:53:03 +0530

    Update managePersonCO.java

--- EBSCustomizations/XXHR/java/xxt/oracle/apps/ak/xxperson/webui/managePersonCO.java
+++ EBSCustomizations/XXHR/java/xxt/oracle/apps/ak/xxperson/webui/managePersonCO.java
@@ -8,6 +8,7 @@
 package xxt.oracle.apps.ak.xxperson.webui;
 //comment
 //comment1
+//comment2
 import oracle.apps.fnd.common.VersionInfo;
 import oracle.apps.fnd.framework.webui.OAControllerImpl;
 import oracle.apps.fnd.framework.webui.OAPageContext;
